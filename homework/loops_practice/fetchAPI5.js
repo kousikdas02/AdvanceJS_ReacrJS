db514121209d0d6306f952f09976eeeaa0649b1f
Kousik Das <kousikdas02@gmail.com>
2023-03-26 21:59:22 +0530

    api loop practice

--- homework/loops_practice/fetchAPI5.js
+++ homework/loops_practice/fetchAPI5.js
@@ -18,7 +18,7 @@ async function fetchUsers() {
     // for (let i = 0; i < Users.length; i++){
     //     console.log(`User geolocation lat: ${Users[i].address.geolocation.lat}\nUser geolocation long: ${Users[i].address.geolocation.long}\nUser city: ${Users[i].address.city}\nUser street: ${Users[i].address.street}\nUser number: ${Users[i].address.number}\nUser zipcode: ${Users[i].address.zipcode}\nUser id: ${Users[i].id}\nUser email: ${Users[i].email}\nUser username: ${Users[i].username}\nUser password: ${Users[i].password}\nUser name: ${Users[i].name.firstname, Users[i].name.lastname}\n`)
     // }
-
+ 
 }
 
 
